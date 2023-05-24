096661333e848a0bfffa293a3c4def64219d4050
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2023-05-24 14:25:13 +0930

    Updating links in uses 😜🚀

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -17,12 +17,12 @@ function ToolsSection({ children, ...props }) {
 function Tool({ title, href, children }) {
   return (
     <Card as="li">
-      <Card.Title as="h3" href={href}>
+      <Card.Title as="h3" href={href} target="_blank" rel="noopener noreferrer">
         {title}
       </Card.Title>
       <Card.Description>{children}</Card.Description>
     </Card>
-  )
+  );
 }
 
 export default function Uses() {
@@ -41,13 +41,13 @@ export default function Uses() {
       >
         <div className="space-y-20">
           <ToolsSection title="Workstation">
-            <Tool title="16” MacBook Pro, M2, 32GB RAM (2022)">
+            <Tool title="16” MacBook Pro, M2, 32GB RAM (2022)" href={"https://www.apple.com/au/shop/buy-mac/macbook-pro/16-inch"}>
               I was using an Intel-based 16” MacBook Pro prior to this and the
               difference is night and day. I’ve never heard the fans turn on a
               single time, even under the incredibly heavy loads I put it
               through with our various launch simulations.
             </Tool>
-            <Tool title="AirPods Max">
+            <Tool title="AirPods Max" href={"https://www.apple.com/au/airpods-max/"}>
               Music plays a vital role in my daily routine,
               whether I`m working on complex coding challenges or
               diving into an engaging study session. With my AirPods Max,
@@ -57,7 +57,11 @@ export default function Uses() {
             </Tool>
           </ToolsSection>
           <ToolsSection title="Development tools">
-            <Tool title="PyCharm Professional">
+            <Tool title="PyCharm Professional"
+                  href={"https://www.jetbrains.com/pycharm/"}
+                  target="_blank"
+                  rel="noopener noreferrer"
+            >
               As a software developer, I need a robust and reliable IDE,
               and PyCharm Professional hits the mark. Its intelligent code
               assistance and extensive range of handy features streamline my
@@ -65,14 +69,14 @@ export default function Uses() {
               The overall result? A significant boost in productivity and a smoother coding
               experience.
             </Tool>
-            <Tool title="DataGrip">
+            <Tool title="DataGrip" href={"https://www.jetbrains.com/datagrip/"}>
               Managing databases is an integral part of my work, and DataGrip is my
               go-to tool for that. It supports various databases and provides smart
               code completion, on-the-fly error detection, and even version control
               system integration. It`s more than just a database tool - it`s a comprehensive
               solution that makes handling databases intuitive and hassle-free.
             </Tool>
-            <Tool title="iTerm2">
+            <Tool title="iTerm2" href={"https://iterm2.com/"}>
               iTerm2 enhances my command-line experience with its array of powerful features.
               The split-pane views, search functionality, and extensive customization options
               it provides all contribute to a superior terminal experience. Whether it`s scripting
@@ -80,13 +84,13 @@ export default function Uses() {
             </Tool>
           </ToolsSection>
           <ToolsSection title="Design">
-            <Tool title="Figma">
+            <Tool title="Figma" href={"https://www.figma.com/"}>
               When it comes to design tools, Figma has become my go-to. Beyond just a design tool,
               it`s our virtual whiteboard for brainstorming, prototyping, and collaboration.
               The seamless real-time collaboration feature is a game-changer,
               enabling us to work together on designs no matter where we are.
             </Tool>
-            <Tool title="MidJourney AI">
+            <Tool title="MidJourney AI" href={"https://www.midjourney.com/home/?callbackUrl=%2Fapp%2F"}>
               When I need original, creative images and graphics for my apps,
               MidJourney AI is my go-to tool. It`s like having a personal graphic
               designer at my disposal. Its AI capabilities help me to generate stunning
@@ -94,13 +98,14 @@ export default function Uses() {
             </Tool>
           </ToolsSection>
           <ToolsSection title="Productivity">
-            <Tool title="JetBrains YouTrack">
-              This is my go-to task management and agile project management tool.
-              It`s remarkably flexible and suits perfectly for my workflow.
-              I can effortlessly manage my tasks, sprints, and backlogs, ensuring that
-              nothing gets lost in the shuffle.
+            <Tool title="Trello" href="https://trello.com">
+              Trello is my go-to task management and project collaboration tool.
+              Its intuitive board, list, and card interface allows me to organize
+              and prioritize my tasks and projects effectively. With features like due dates,
+              checklists, labels, and integrations, Trello helps me stay on top of my work
+              and collaborate with team members seamlessly.
             </Tool>
-            <Tool title="ChatGPT">
+            <Tool title="ChatGPT" href={"https://chat.openai.com/"}>
               I find ChatGPT incredibly useful for brainstorming, code review,
               and as a writing assistant. It`s like having an AI-powered co-worker who`s always
               ready to provide insights and assist with the development process. It really has become

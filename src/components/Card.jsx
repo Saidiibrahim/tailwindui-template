a882bc3b11bc7460d401da4f50e50565023114b8
Ibrahim Saidi <ibrahim.aka.ajax@gmail.com>
2023-05-25 10:44:53 +0930

    Updated links in uses 😜🚀

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,7 +28,7 @@ Card.Link = function CardLink({ children, ...props }) {
   return (
     <>
       <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
-      <Link {...props} >
+      <Link {...props}>
         <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
       </Link>
@@ -39,7 +39,13 @@ Card.Link = function CardLink({ children, ...props }) {
 Card.Title = function CardTitle({ as: Component = 'h2', href, children }) {
   return (
     <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-      {href ? <Card.Link href={href}>{children}</Card.Link> : children}
+      {href ? (
+        <Card.Link href={href} target="_blank" rel="noopener noreferrer">
+          {children}
+        </Card.Link>
+      ) : (
+        children
+      )}
     </Component>
   )
 }
